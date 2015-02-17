8394018f1aaed0b9ad90e84560f53449a47596fd
jf2944 <jfantiguap-2944@yahoo.com>
2015-02-17 12:34:23 -0500

    Initial version of matrix caching function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+####################################################
+# The following functions provide an environment for 
+# creating, caching and retrieving the inverse of
+# a square matrix. 
+# This speeds up retrieval of the inverted matrix after
+# the first time, by returning the cached inverse,
+# rather than executing the matrix inversion algorithm
+# every time.
+#
+# Usage:
+#   initialize using:
+#       myFunctionList <- makeCacheMatrix (myMatrix),
+#   then call for each access to the the inverted matrix: 
+#       cacheSolve(myFunctionList)
+#       
+####################################################
 
-## Write a short comment describing this function
+# makeCacheMatrix creates and returns the list of functions 
+# to operate on the cache of the inverse matrix. 
+#
+# The internal environment includes the list of functions, 
+# the original matrix, the inverse of the matrix.
+#
+# Usage:  myFunctionList <- makeCacheMatrix (myMatrix)
+#         where
+#           myFunctionList is the returned list of functions, 
+#           myMatrix is the original matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m <- NULL
+    setMatrix <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    getMatrix <- function() x
+    setInvertedMatrix <- function(mat) m <<- mat
+    getInvertedMatrix <- function() m
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix,
+         setInvertedMatrix = setInvertedMatrix,
+         getInvertedMatrix = getInvertedMatrix)
+    
 }
 
-
-## Write a short comment describing this function
+# cacheSolve retrieves the inverted matrix from the cache.
+# If the cache is empty, the original matrix is inverted,
+# the result is stored in the cache, as well as returned.
+#
+# Usage:  myMatrixInverse <- cacheSolve(myFunctionList)
+#         where
+#           myMatrixInverse is the inverse of the original matrix,
+#           myFunctionList is the list of functions that are
+#             associated with the original matrix after 
+#             makeCacheMatrix() has been called, 
+#         
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInvertedMatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    m <- solve(x$getMatrix())
+    x$setInvertedMatrix(m)
+
+    return(m)
 }
+
